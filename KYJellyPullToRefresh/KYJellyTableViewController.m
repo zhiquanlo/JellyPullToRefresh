869849880ed7ad1b0aa7a8189e6aba2e357cc58f
李志权 <lizhiquan@lizhiquandeiMac.local>
2017-07-04 15:37:10 +0800

    aa

---
+++ KYJellyPullToRefresh/KYJellyTableViewController.m
@@ -0,0 +1,160 @@
+//
+//  KYJellyTableViewController.m
+//  KYJellyPullToRefresh
+//
+//  Created by Kitten Yang on 2/7/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+
+
+
+#define jellyHeaderHeight 300
+#import "KYJellyTableViewController.h"
+
+@interface KYJellyTableViewController ()
+
+@property (nonatomic,strong) CADisplayLink *displayLink;
+
+
+
+@end
+
+@implementation KYJellyTableViewController{
+
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blackColor];
+    self.title  = @"弹性小球下拉刷新";
+    self.tableView.allowsSelection = YES;
+    self.tableView.delegate  =self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return 8;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"JellyCell" forIndexPath:indexPath];
+    NSString *imgName = [NSString stringWithFormat:@"cellImg%ld",indexPath.row + 1];
+    UIImageView *cellImgView = (UIImageView *)[cell.contentView viewWithTag:101];
+    cellImgView.image = [UIImage imageNamed:imgName];
+    
+    return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
+
+
+//判断滚动方向向上还是向下
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    
+    if (scrollView.contentOffset.y > -64.5) {
+        if (self.jellyView.isLoading == NO) {
+            [self.jellyView removeFromSuperview];
+            self.jellyView = nil;
+            [self.displayLink invalidate];
+            self.displayLink = nil;
+        }
+        
+        return;
+    }
+
+    if (self.displayLink == nil && (-scrollView.contentOffset.y - 64.5) > 0) {
+        self.jellyView = [[JellyView alloc]initWithFrame:CGRectMake(0, -jellyHeaderHeight , [UIScreen mainScreen].bounds.size.width, jellyHeaderHeight)];
+        self.jellyView.backgroundColor = [UIColor clearColor];
+        [self.view insertSubview:self.jellyView aboveSubview:self.tableView];
+        
+        
+        self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayLinkAction:)];
+        [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+    }else if ((-scrollView.contentOffset.y - 64.5) < 0){
+        [self.jellyView removeFromSuperview];
+        self.jellyView = nil;
+        [self.displayLink invalidate];
+        self.displayLink = nil;
+    }
+
+}
+
+
+//松手的时候
+-(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+
+    CGFloat offset = -scrollView.contentOffset.y - 64.5;
+    if (offset >= 130) {
+        
+        self.jellyView.isLoading = YES;
+        
+        [UIView animateWithDuration:0.3 delay:0.0f usingSpringWithDamping:0.4f initialSpringVelocity:0.0f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            
+            self.jellyView.controlPoint.center = CGPointMake(self.jellyView.userFrame.size.width / 2, jellyHeaderHeight);
+            NSLog(@"self.jellyView.controlPoint.center:%@",NSStringFromCGPoint(self.jellyView.controlPoint.center));
+            
+            self.tableView.contentInset = UIEdgeInsetsMake(130+64.5, 0, 0, 0);
+        } completion:^(BOOL finished) {
+            [self performSelector:@selector(backToTop) withObject:nil afterDelay:2.0f];
+        }];
+    }
+    
+}
+
+//动画结束，删除一切
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+
+    if (self.jellyView.isLoading == NO) {
+        [self.jellyView removeFromSuperview];
+        self.jellyView = nil;
+        [self.displayLink invalidate];
+        self.displayLink = nil;
+    }
+
+}
+
+//跳到顶部的方法
+-(void)backToTop{
+    
+    [self.jellyView.layer removeAnimationForKey:@"rotationAnimation"];
+    [UIView animateWithDuration:0.3 delay:0.0f usingSpringWithDamping:0.4f initialSpringVelocity:0.0f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.tableView.contentInset = UIEdgeInsetsMake(64.5, 0, 0, 0);
+    } completion:^(BOOL finished) {
+        self.jellyView.isLoading = NO;
+        [self.jellyView removeFromSuperview];
+        self.jellyView = nil;
+        [self.displayLink invalidate];
+        self.displayLink = nil;
+    }];
+}
+
+//持续刷新屏幕的计时器
+-(void)displayLinkAction:(CADisplayLink *)dis{
+    
+    self.jellyView.controlPointOffset = (self.jellyView.isLoading == NO)? (-self.tableView.contentOffset.y - 64.5) : (self.jellyView.controlPoint.layer.position.y - self.jellyView.userFrame.size.height);
+    [self.jellyView setNeedsDisplay];
+   
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,81 @@
+
+<p align="left" >
+  <img src="logo.png" alt="KYJellyPullToRefresh" title="KYJellyPullToRefresh">
+</p>
+
+
+![CocoaPods Version](https://img.shields.io/badge/pod-v1.1.0-brightgreen.svg)
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+![Platform](https://img.shields.io/badge/platform-iOS-red.svg)
+
+
+
+![](ball.gif)
+
+
+##Usage
+
+`pod 'KYJellyPullToRefresh', '~> 1.1.0'`
+
+
+##How To Use
+
+Firstly,in your ViewController or TableViewController,initiate the Jellview and the "displayLink"  in the `- (void)scrollViewDidScroll:(UIScrollView *)scrollView`  method:
+Like this:
+```objective-c
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    if (self.displayLink == nil && (-scrollView.contentOffset.y - 64.5) > 0) {
+        self.jellyView = [[JellyView alloc]initWithFrame:CGRectMake(0, -jellyHeaderHeight , [UIScreen mainScreen].bounds.size.width, jellyHeaderHeight)];
+        self.jellyView.backgroundColor = [UIColor clearColor];
+        [self.view insertSubview:self.jellyView aboveSubview:self.tableView];
+        self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayLinkAction:)];
+        [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+    }
+}
+```
+
+Then you should change the controlPoint of the Bezier Path
+
+```objective-c
+-(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+
+    CGFloat offset = -scrollView.contentOffset.y - 64.5;
+    if (offset >= 130) {
+        self.jellyView.isLoading = YES;
+        [UIView animateWithDuration:0.3 delay:0.0f usingSpringWithDamping:0.4f initialSpringVelocity:0.0f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            
+            self.jellyView.controlPoint.center = CGPointMake(self.jellyView.userFrame.size.width / 2, jellyHeaderHeight);
+            NSLog(@"self.jellyView.controlPoint.center:%@",NSStringFromCGPoint(self.jellyView.controlPoint.center));
+            self.tableView.contentInset = UIEdgeInsetsMake(130+64.5, 0, 0, 0);
+        } completion:^(BOOL finished) {
+            [self performSelector:@selector(backToTop) withObject:nil afterDelay:2.0f];
+        }];
+    }
+}
+
+```
+
+Lastest,remove everything:
+
+```objective-c
+//动画结束，删除一切
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+
+    if (self.jellyView.isLoading == NO) {
+        [self.jellyView removeFromSuperview];
+        self.jellyView = nil;
+        [self.displayLink invalidate];
+        self.displayLink = nil;
+    }
+}
+
+```
+
+
+Intro:  [http://kittenyang.com/cadisplaylinkanduibezierpath/](http://kittenyang.com/cadisplaylinkanduibezierpath/)
+
+
+##License
+This project is under MIT License. See LICENSE file for more information.
+
